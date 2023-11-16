3a56bad5a6f811f1868ca17cf237a4a0e60d927c
Ibzi12 <128941663+Ibzi12@users.noreply.github.com>
2023-11-17 06:40:02 +1100

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -13,3 +13,44 @@ document.getElementById("output").innerHTML=row;
 
     window.location="quiz_game_page.html";
 }
+question_turn="player1";
+answetr_turn="player2";
+
+function check()
+{
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answeranswer==actual_answer)
+    {
+        if(answer_turn=="player1")
+        {
+            update_player1_score=player1_score+1;
+            document.getElementById("player1_score").innerHTML=player1_score;
+        }
+        else
+        {
+            update_player2_score=player2_score+1;
+            document.getElementById("player2_score").innerHTML=player2_score;
+        }
+    }
+    if(question_turn=="player1")
+    {
+        question_turn="player2"
+        document.getElementById("player_question").innerHTML="Question turn-"+player2_name;
+    }
+    else
+    {
+        question_turn="player1"
+        document.getElementById("player_question").innerHTML="Question Turn-"+player1_name;
+    }
+    if(question_turn=="player1")
+    {
+        question_turn="player2"
+        document.getElementById("player_answer").innerHTML="Question turn-"+player2_name;
+    }
+    else
+    {
+        question_turn="player1"
+        document.getElementById("player_answer").innerHTML="Question Turn-"+player1_name;
+    }
+    document.getElementById("output").innerHTML="";
+}
